a829e4381a54b2177f299993032cf435c2b07292
AlyaMirzaSafira <alyamirza96@gmail.com>
2024-05-26 19:47:05 +0700

    tugas_pertemuan6

---
+++ Pertemuan6_Laravel/try1_laravel/resources/views/view/perkenalan.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>INTRODUCE</title>
+        <style>
+            body {
+                font-family: Arial, sans-serif;
+                background-color: beige;
+                color: black;
+                margin: 0%;
+                padding: 0%;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                height: 100vh;
+            }
+            .container {
+                background: white;
+                padding: 20px;
+                border-radius: 10px;
+                box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+                text-align: center;
+            }
+            h1 {
+                color: aqua;
+            }
+            p {
+                font-size: 18px;
+                line-height: 1.6;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <h1>HELO EVERYONE, LET ME INTRODUCE MY SELF </h1>
+            <p>my name is Alya Mirza Safira, U can call me Alya</p>
+            <p>I was born in Malang, on 8 November 2002</p>
+            <p>I'm twenty one years old</p>
+            <p>Now I live in Surabaya, cause I study there</p>
+            <p>I'm a sixth semester student at Universitas Pembangunan Nsional Veteran Jwa Timur, majoring in data science</p>
+            <p>I Like badminton and watching movies</p>
+            <p>Now I'm so interested to learn about machine learning, artificial intelligence, IOT, and all about data (data engineer, data analyst, and data science)</p>
+        </div>
+    </body>
+</html>
